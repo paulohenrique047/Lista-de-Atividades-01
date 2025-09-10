23a3bbf79df21d582e232365715ffe6abd86c2df
Paulo Henrique <paulohenriquenmv047@gmail.com>
2025-09-10 09:21:17 -0300

    atividade dos votos

---
+++ Ativ11/Ativ11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO.Pipes;
+
+Console.WriteLine("Em que ano estamos?");
+int ano = int.Parse(Console.ReadLine());
+Console.WriteLine("Qual a sua ano você nasceu?");
+int nas = int.Parse(Console.ReadLine());
+int ida = ano - nas;
+if (ida < 16)
+{
+    Console.WriteLine("Você não é obrigado a votar esse ano");
+}
+else
+{
+    if (ida < 69 && ida > 18 )
+    {
+        Console.WriteLine("Você é obrigdo a votar");
+    }
+    else
+    {
+        Console.WriteLine("Seu voto é facultativo");
+    }
+}
