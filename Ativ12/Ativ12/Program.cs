d5e65ea4dc27ad865ad837ae2f508303cd21cec1
Paulo Henrique <paulohenriquenmv047@gmail.com>
2025-09-10 09:28:21 -0300

    atividade para ver qual número é maior

---
+++ Ativ12/Ativ12/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Digite o primeiro valor");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o segundo valor");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine($"O maior número é {num1}");
+}
+else
+{
+    Console.WriteLine($"O maior número é {num2}");
+}
+
