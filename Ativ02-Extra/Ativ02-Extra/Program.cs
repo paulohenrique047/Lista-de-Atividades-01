cc57b9b205d3bc7034b3fbf0d840fd72ebdf2255
Paulo Henrique <paulohenriquenmv047@gmail.com>
2025-09-10 11:05:27 -0300

    atividade de calcular a área da parede e a tinta

---
+++ Ativ02-Extra/Ativ02-Extra/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Qual a largura da parede em metros?");
+double lar = double.Parse(Console.ReadLine());
+Console.WriteLine("Qual a largura da parede em metros?");
+double alt = double.Parse(Console.ReadLine());
+double are = alt * lar;
+Console.WriteLine($"O total a ser pago pela tinta é R${are / 2}");
+
