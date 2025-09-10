0519d551dac84474acef78da2b450e76da2198d8
Paulo Henrique <paulohenriquenmv047@gmail.com>
2025-09-10 08:53:03 -0300

    atividade média de notas

---
+++ Ativ10/Ativ10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http.Headers;
+
+Console.WriteLine("Qual foi a sua nota na 1ª avaliação?");
+double num1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Qual foi a sua nota na 2ª avaliação?");
+double num2 = double.Parse(Console.ReadLine());
+double Media = (num1 + num2) / 2;
+if (Media < 6)
+{
+    Console.WriteLine($"Sua média foi {Media}, você não foi aprovado!");
+}
+else
+{
+    Console.WriteLine($"Sua média foi {Media}, você foi aprovado!");
+}
