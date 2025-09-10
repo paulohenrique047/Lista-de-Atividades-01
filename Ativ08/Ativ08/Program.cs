f2278238e757c98f1d2b18bbd2f733d2f04cf1d2
Paulo Henrique <paulohenriquenmv047@gmail.com>
2025-09-10 09:01:45 -0300

    atividade de ver se o número é positivo ou negativo

---
+++ Ativ08/Ativ08/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Digite um valor:");
+double num = double.Parse(Console.ReadLine());
+if (num > 0)
+{
+    Console.WriteLine($"{num} é positivo.");
+}
+else
+{
+    Console.WriteLine($"{num} é negativo.");
+}
