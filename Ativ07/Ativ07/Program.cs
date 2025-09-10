ea09dc995b773903a391ebc67c5a91b05d10c876
Paulo Henrique <paulohenriquenmv047@gmail.com>
2025-09-10 08:30:50 -0300

    atividade maior ou menor que 10

---
+++ Ativ07/Ativ07/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Digite um número");
+int num = int.Parse(Console.ReadLine());
+if (num > 10)
+{
+    Console.WriteLine("É MAIOR QUE 10!");
+}
+else
+{
+    Console.WriteLine("NÃO É MAIOR QUE 10!");
+}
+
