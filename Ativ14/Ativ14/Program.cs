278274fb981bd2311bbe7e96afbc502ffe7506c4
Paulo Henrique <paulohenriquenmv047@gmail.com>
2025-09-16 08:36:34 -0300

    Atividade de dividir números por 11 e resto 5

---
+++ Ativ14/Ativ14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ImprimirNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um valor para N: ");
+            int n = int.Parse(Console.ReadLine());
+            if (n > 0)
+            {
+                Console.WriteLine($"\nValores inteiros entre 1 e {n}:");
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("O valor de N deve ser maior que zero!");
+            }
+            Console.WriteLine("\nPressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ativ15/Ativ15/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Números entre 1000 e 2000 que divididos por 11 dão resto 5:");
+        int contador = 0;
+        for (int numero = 1000; numero <= 2000; numero++)
+        {
+            if (numero % 11 == 5)
+            {
+                Console.Write($"{numero} ");
+                contador++;
+
+                if (contador % 10 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+        Console.WriteLine($"\n\nTotal de números encontrados: {contador}");
+    }
+}
