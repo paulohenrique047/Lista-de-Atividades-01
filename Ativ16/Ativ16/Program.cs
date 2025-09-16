5b28514b0625ca014c349b53ecff9b1f9cdc5354
Paulo Henrique <paulohenriquenmv047@gmail.com>
2025-09-16 08:53:09 -0300

    Atividade fatorial

---
+++ Ativ16/Ativ16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número para calcular o fatorial: ");
+        int n = int.Parse(Console.ReadLine());
+        if (n < 0)
+        {
+            Console.WriteLine("Não existe fatorial de número negativo!");
+        }
+        else
+        {
+            long fatorial = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                fatorial *= i;
+            }
+            Console.WriteLine($"{n}! = {fatorial}");
+        }
+        Console.ReadKey();
+    }
+}
